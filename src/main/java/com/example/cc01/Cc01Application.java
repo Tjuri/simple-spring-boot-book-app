63bf952c6c0cd64fced15cb6c08f05814e76271c
Michi Privat <michiprivat@Michaels-MBP-2.fritz.box>
2020-04-19 20:48:40 +0200

    init

---
+++ src/main/java/com/example/cc01/Cc01Application.java
@@ -0,0 +1,13 @@
+package com.example.cc01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cc01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cc01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cc01/Cc01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cc01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cc01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
